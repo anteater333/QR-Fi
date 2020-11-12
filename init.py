83c89b745a4a9cf71c49706a74be3d01f6c8dde6
HardM00N <baejaehyun95@gmail.com>
2020-11-13 00:55:13 +0900

    파이썬에서 QR코드 저장하는 방식으로는 완성 코드

--- init.py
+++ init.py
@@ -14,8 +14,13 @@ def main():
         password = request.form['password']
         protocol = request.form['protocol']
         
-        qr = qrcode.make(F'WIFI:S:{ssid};T:{protocol};P:{password};;')
-        qr.save("./static/qr.png")
+        qr = qrcode.QRCode(version=12, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=6, border=4)
+        qr.add_data(F'WIFI:S:{ssid};T:{protocol};P:{password};;')
+        #qr = qrcode.make(F'WIFI:S:{ssid};T:{protocol};P:{password};;')
+        #qr.save("./static/qr.png")
+        qr.make()
+        qr1 = qr.make_image(fill_color="#000000", back_color="#ffffff")
+        qr1.save("./static/qr.png")
         global temp_qr
         temp_qr=qr
 
@@ -27,11 +32,11 @@ def result():
     if request.method == 'POST':
         fill = request.form['fill_color']
         back = request.form['back_color']
-        #img = temp_qr.make_image(fill_color = fill, back_color = back)
-        #img.save("./static/qr.png")
+        img = temp_qr.make_image(fill_color = fill, back_color = back)
+        img.save("./static/qr.png")
 
         #return render_template('result.html', image_file='static/qr.png')
-        return render_template('result.html', fill_color=fill, back_color=back)
+        return render_template('result.html', fill_color=fill, back_color=back, image_file='static/qr.png', name=temp_ssid)
     return render_template('result.html', image_file='static/qr.png', name=temp_ssid)
 
 @app.route('/about')
@@ -41,6 +46,13 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.after_request
+def set_response_headers(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
 
 if __name__ == "__main__":
     app.run()
